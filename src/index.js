3a1b1a049fc2ea18eac53ebca668d8b3e0a196b6
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-08-26 16:13:28 -0400

    Test lint

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import "./styles.css"
 import { test } from "./test.js";
 
 console.log(test);
+
+let yell = () => console.log("HELLO!") 
+const variable = "yes"
+
+let i = ["I", , "hello", "no"];
+
+console.log(i)
+
+
+
+
+
+
