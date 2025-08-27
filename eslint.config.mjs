6ff13ead6d81df81a724f87f31de6aff354926b4
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-08-27 13:17:32 -0400

    Import Prettier and add to export obj

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat"
 
 export default defineConfig([
-	{ 
-		files: ["**/*.{js,mjs,cjs}"], 
-		plugins: { 
-			js
-		}, 
-		extends: ["js/recommended"], 
-		languageOptions: { 
-			globals: globals.browser 
-		} 
-	},
-	{
-		rules: {
-		"no-unused-vars": "warn",
-		"no-undef": "warn",
-		},
-	},
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  }, eslintConfigPrettier,
 ]);
