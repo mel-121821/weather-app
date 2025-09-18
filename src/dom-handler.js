5e020aaefce9de324110aae2f8822679191eaa4d
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-09-18 12:12:46 -0400

    Add query selectors for 7 day forcast and organize remaining selectors into groups. Begin creating a fn() to update 7 day display and add support fn()s to get the weekday from fetch data. Also add note regarding a bug that is caused by a "0" as the first char in the MM value of date

--- src/dom-handler.js
+++ src/dom-handler.js
@@ -2,15 +2,31 @@
 import { format } from 'date-fns'
 import { requestHandler } from './request-handler'
 
+// Query selectors
+
+// General
+const currentDate = document.querySelector('.date > p')
 const allUnitDisplays = document.querySelectorAll('span.units')
 const main = document.querySelector('.main')
-const currentLocation = document.querySelector('.location > p')
-const currentDate = document.querySelector('.date > p')
-const currentTemp = document.querySelector('.temp > p > span.val')
-const currentConditions = document.querySelector('.conditions .para > p')
-const currentPrecip = document.querySelector('.precip .percent .val')
-const currentPrecipIcon = document.querySelector('.precip .icon img')
-const currentDescription = document.querySelector('.description > p')
+
+// Current weather display
+const current_Location = document.querySelector('.location > p')
+const current_Temp = document.querySelector('.temp > p > span.val')
+const current_Conditions = document.querySelector('.conditions .para > p')
+const current_PrecipPercentage = document.querySelector(
+    '.current .precip .percent .val',
+)
+const current_PrecipIcon = document.querySelector('.precip .icon img')
+const current_Description = document.querySelector('.description > p')
+
+// 7 day forcast display
+const dayOfWeek = document.querySelectorAll('.weekday p')
+const forcast_ConditionsIcon = document.querySelectorAll('.group-1 .icon img')
+const forcast_PrecipIcon = document.querySelectorAll('.precip-icon img')
+const forcast_PrecipPercentage = document.querySelectorAll(
+    '.group-2 .precip .val',
+)
+const forcast_ConditionsPara = document.querySelectorAll('.forcast .conditions')
 
 class domHandler {
     static changeUnits() {
@@ -30,19 +46,25 @@ class domHandler {
     // Note: assigning a string to textContent erases the existing content of the element, including the span. https://stackoverflow.com/questions/75430221/im-not-seeing-span-tags-in-dom-when-adding-them-via-javascript-loop
 
     static async displayCurrent(data) {
-        console.log(currentTemp)
+        console.log(current_Temp)
         currentDate.textContent = domHandler.getCurrentDate()
-        currentLocation.textContent = data.resolvedAddress
-        currentTemp.textContent = data.temp
-        currentConditions.textContent = data.conditions
-        currentPrecip.textContent = data.precip
-        currentPrecipIcon.src = await domHandler.setCurrentPrecipIcon(data)
-        currentDescription.textContent = data.description
+        current_Location.textContent = data.resolvedAddress
+        current_Temp.textContent = data.temp
+        current_Conditions.textContent = data.conditions
+        current_PrecipPercentage.textContent = data.precip
+        current_PrecipIcon.src = await domHandler.setPrecipIcon(data)
+        current_Description.textContent = data.description
+    }
+
+    static displayForcast(data) {
+        data.forEach((day, index) => {
+            // console.log(`Day ${index} = ${day.datetime}`)
+            dayOfWeek[index].textContent = domHandler.getWeekday(day.datetime)
+        })
     }
 
     static async updateBgImg(data) {
         const iconName = data.icon
-        console.log(iconName)
         try {
             const bgImg = await import(`../img/${iconName}.jpg`)
             console.log(bgImg.default)
@@ -54,10 +76,9 @@ class domHandler {
         }
     }
 
-    static async setCurrentPrecipIcon(data) {
+    static async setPrecipIcon(data) {
         const iconName = data.preciptype
         let iconSrc
-        console.log(iconName)
         try {
             const precipIcon = await import(
                 `../icon/preciptype/${iconName[0]}.svg`
@@ -75,8 +96,33 @@ class domHandler {
 
     static getCurrentDate() {
         const today = format(new Date(new Date()), "EEEE', ' MMMM d', ' yyyy")
-        console.log(new Date().toISOString().substring(0, 10))
         return today
+    }
+
+    // Note: code doesn't work properly when the first character in the month is a '0' it causes the outputted day to be decreased by 1
+    // Ex. 1: Date submitted: "2025-09-19" becomes "2025-09-18"
+    // Ex. 2: Date submitted: "2025-04-07" becomes "2025-04-06"
+    // This does not seem to be an issue with day characters
+
+    static getWeekday(data) {
+        const formattedDate = domHandler.formatDateData(data)
+        console.log(formattedDate)
+        const date = format(new Date(formattedDate), 'EEEE')
+        return date
+    }
+
+    static formatDateData(data) {
+        let formattedDate = data
+        if (data.charAt(5) === '0') {
+            formattedDate = data
+                .split('')
+                .slice(0, 5)
+                .join('')
+                .concat(data.split('').slice(-4).join(''))
+        } else {
+            console.log('date was not modified')
+        }
+        return formattedDate
     }
 }
 
