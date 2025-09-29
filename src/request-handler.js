b5c1ef65d4d9877df73f6dea6a0ed1b1d882ac59
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-09-29 11:13:35 -0400

    Remove completed TODOs, notes and console.logs

--- src/request-handler.js
+++ src/request-handler.js
@@ -5,14 +5,12 @@ class requestHandler {
 
     static currentLocation = 'Ottawa+ON+Canada'
 
-    // Note: getters/setters behave like variables, not functions. Do not try to call them or you will get an error!
     static get units() {
         return this.currentUnits
     }
 
     static setUnits() {
         this.currentUnits = this.currentUnits === 'metric' ? 'us' : 'metric'
-        console.log(this.currentUnits)
     }
 
     static get location() {
@@ -20,11 +18,7 @@ class requestHandler {
     }
 
     static set location(inputs) {
-        // DONE: fix to accept city/provice/country
-        // DONE: add code to handle undefined values > made city and region required fields instead
-        console.log(inputs)
         this.currentLocation = inputs.filter(Boolean).join('+')
-        console.log(this.currentLocation)
     }
 }
 
